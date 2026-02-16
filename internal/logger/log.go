5b14e92861174d66df7cd42d396e3d3c11ddb229
badreddine <153949545+0xbdr@users.noreply.github.com>
2026-02-16 12:53:14 +0000

    Lmfao it logs to the cnfg dir
    Add comment explaining log file location in config directory

--- internal/logger/log.go
+++ internal/logger/log.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	var logfile, err = os.Open("~/.config/gohttp/log")
+	var logfile, err = os.Open("~/.config/gohttp/log") //lmao why the log is in the config dir
 	if err != nil {
 		logfile , err = os.Create("~/.config/gohttp/log")
 	}	
